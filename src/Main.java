adc49c613b6dfe66c8b0bb9c5cf5c1cca995c9a1
Pururucs <arthur_pereira26@icloud.com>
2025-10-01 21:26:14 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Arthur";
+        String sobrenome = "Pereira";
+        String nomeCompleto = nome + " " + sobrenome;
+        System.out.println("Nome do cliente: " + nomeCompleto);
+        double altura = 1.80;
+        System.out.println("A altura do cliente: " + altura);
+        int idade = 14;
+        System.out.println("A idade do cliente é: " + idade);
+
+
+    }
+}
